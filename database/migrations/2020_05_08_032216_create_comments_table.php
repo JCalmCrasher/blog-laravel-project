6dd3ba641228d9b9061b0daeccadbdfe350fa13c
Joshua Osagie <josh001pro@gmail.com>
2020-05-08 04:23:56 +0100

    .

--- database/migrations/2020_05_08_032216_create_comments_table.php
+++ database/migrations/2020_05_08_032216_create_comments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCommentTable extends Migration
+class CreateCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -18,6 +18,7 @@ class CreateCommentTable extends Migration
             $table->integer('post_id');
             $table->string('commenter_name');
             $table->string('content');
+            $table->char('approved', 1);
             $table->timestamps();
         });
     }
