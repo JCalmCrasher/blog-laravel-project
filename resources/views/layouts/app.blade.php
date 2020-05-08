878f3869227ef0b47b0ed9b052e1d0011d3f969a
Joshua Osagie <josh001pro@gmail.com>
2020-05-08 04:06:00 +0100

    .

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,9 +56,7 @@
                             <!-- Authentication Links -->
                             @guest
                             <li class="nav-item">
-                                <a
-                                    class="nav-link"
-                                    href="{{ route('login') }}"
+                                <a class="nav-link" href="{{ route('login') }}"
                                     >{{ __("Login") }}</a>
                             </li>
                             @if (Route::has('register'))
